01f6e7926271794ded87bc1fef588effcb5073c1
krisode <huyttdse140080@fpt.edu.vn>
2021-01-19 22:34:47 +0700

    Create folder: enums, interfaces, implements

---
+++ ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IRepository<T>
+    {
+        T GetById(int id);
+
+        T Get(Expression<Func<T, bool>> where);
+
+        void Update(T entity);
+
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        void Delete(Expression<Func<T, bool>> where);
+
+        IEnumerable<T> GetAll();
+
+        IEnumerable<T> GetAll(params Expression<Func<T, object>>[] includes);
+
+        IEnumerable<T> GetList(Expression<Func<T, bool>> where);
+
+        IQueryable<T> _GetList(Expression<Func<T, bool>> where);
+
+        IQueryable<T> _GetList(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] includes);
+
+    }
+}
