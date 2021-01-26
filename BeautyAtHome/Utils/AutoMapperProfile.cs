b6b4314c429d2ea7ed77f6f7e19f6de2dd02b843
thehienvnag <thehienvnag@gmail.com>
2021-01-26 16:01:47 +0700

    [HienHT][Refactor_Code_Paging_Fixed] Fixed AutoMapperProfile

--- BeautyAtHome/Utils/AutoMapperProfile.cs
+++ BeautyAtHome/Utils/AutoMapperProfile.cs
@@ -12,12 +12,21 @@ namespace BeautyAtHome.Utils
             CreateMap<ServiceCM, Service>();
             CreateMap<Service, ServiceVM>();
             CreateMap<Service, ServicePagingSM>();
-            CreateMap<ServiceType, ServiceTypePagingSM>();
-            CreateMap<Account, AccountPagingSM>();
-            CreateMap<Gallery, GalleryPagingSM>();
             CreateMap<ServiceVM, Service>();
             CreateMap<ServiceCM, Service>();
             CreateMap<ServiceUM, Service>();
+            #endregion
+
+            #region AutoMapper AccountViewModel
+            CreateMap<Account, AccountPagingSM>();
+            #endregion
+
+            #region AutoMapper GalleryViewModel
+            CreateMap<Gallery, GalleryPagingSM>();
+            #endregion
+
+            #region AutoMapper ServiceTypeViewModel
+            CreateMap<ServiceType, ServiceTypePagingSM>();
             #endregion
         }
     }
